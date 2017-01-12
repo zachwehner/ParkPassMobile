36bb52f9c9efe8a496073762b0dc06a8c638d481
zachwehner <zachwehner@gmail.com>
2017-01-11 21:37:18 -0600

    Fix

--- ParkPass/Models/AmenitiesListItem.cs
+++ ParkPass/Models/AmenitiesListItem.cs
@@ -1,9 +1,12 @@
-﻿using System;
+﻿
+using System;
 namespace ParkPass.Models
 {
 	public class AmenityCellAmenitiesListItem
 	{
 		public string Name { get; set; }
+
+
 
 		public string Description { get; set; }
 
